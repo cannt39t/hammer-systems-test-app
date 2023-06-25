a4f160877736870d2c1017a259645c85a5bda1c7
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-06-25 23:46:15 +0300

    Delete comments

--- HammerSystemsTestApp/HammerSystemsTestApp/Application/SceneDelegate.swift
+++ HammerSystemsTestApp/HammerSystemsTestApp/Application/SceneDelegate.swift
@@ -13,7 +13,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//        URLCache.shared.removeAllCachedResponses()
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
